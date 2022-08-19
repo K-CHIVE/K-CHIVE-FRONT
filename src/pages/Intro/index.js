adc385e88d042490e341902ff09e7c1370fbadb3
youngmin-woo <bowwow515@hanyang.ac.kr>
2022-08-20 01:27:40 +0900

    Add: link connection from intro to contents

--- src/pages/Intro/index.js
+++ src/pages/Intro/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 import { Logo } from "component/Logo";
+import { Link } from "react-router-dom";
 
 import { IntroWrapper } from "./styles";
 
 const Intro = () => {
   return (
     <>
+    <Link to="/search">
       <IntroWrapper>
         <Logo width={177} height={50} />
       </IntroWrapper>
+    </Link>
     </>
   );
 };
